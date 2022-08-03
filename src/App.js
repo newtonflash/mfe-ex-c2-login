5b8c8ebc3b9f88bde8cf9502b12c5f2257c6b401
Mahesh Madhusudan <mahesh.madhusudan@tesco.com>
2022-08-03 19:23:00 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 
+import React, { useState } from 'react';
 import './App.css';
+import users from "./users.json"
 
 function App() {
+  const [uname, setUname] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("Guest");
+  const loginhandler = (e) => {
+    const loggedUser = users.find((user) => user.username === uname && user.password === password);
+    if(loggedUser){
+      setFirstName(loggedUser.firstname);
+    }
+    else {
+      setFirstName("Guest");
+    }
+    
+  }
   return (
     <div className="App">
-    `<div>
-        <form action="" method="post">
-          <input id="uname" type="text" placeholder="Enter user name"></input>
-
-          <input id="psswd" type="password" placeholder="Enter password"></input>
-
-          <input class="primary-btn" type="submit" value="Login"></input>
-        </form>
+      <div className="welcomeMessage">
+          <p>Welcome, {firstName}</p>
+      </div>
+     <div>
+          <input id="uname" type="text" placeholder="Enter user name" onChange={(e)=>setUname(e.target.value)}></input>
+          <input id="psswd" type="password" placeholder="Enter password" onChange={(e)=>setPassword(e.target.value)}></input>
+          <input className="primary-btn" type="submit" value="Login" onClick={loginhandler}></input>
       </div>
     </div>
   );
