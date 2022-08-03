48ba0520bc872fd74db7d5721ddf5b931cce7494
Mahesh Madhusudan <mahesh.madhusudan@tesco.com>
2022-08-03 17:33:47 +0530

    adding init application

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    `<div>
+        <form action="" method="post">
+          <input id="uname" type="text" placeholder="Enter user name"></input>
+
+          <input id="psswd" type="password" placeholder="Enter password"></input>
+
+          <input class="primary-btn" type="submit" value="Login"></input>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
